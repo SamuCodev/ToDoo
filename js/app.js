8d0e3a03d15e7ec7ff9c3abf3e0a98271d8387da
Samuel David <samuelddavid0530@gmail.com>
2026-05-19 22:33:22 -0500

    Se implemento validacion y feedback visual para el usuario

--- js/app.js
+++ js/app.js
@@ -1,24 +1,20 @@
 let tareas = JSON.parse(localStorage.getItem("tareas")) || []
-crearTarea(tareas)
 //Modal
 let btnAbrirModal = document.querySelector(".btn-agg")
 let btnCerrarModal = document.querySelector(".btn-cerrar-modal")
 let btnAgregarTarea = document.querySelector(".btn-agg-modal")
 let modalOculto = document.querySelector(".modal")
-
 //Inputs Modal
 let titleTarea = document.querySelector(".title-tarea-modal")
 let descTarea = document.querySelector(".desc-tarea-modal")
 let timeTarea = document.querySelector(".time-tarea-modal")
-
 //filtros
-
 let btnTodos = document.querySelector(".btn-todos")
 let btnCompletados = document.querySelector(".btn-completed")
 let btnNoCompletados = document.querySelector(".btn-incompleted")
 
 //Funcion que crea y actualiza las tareas actuales
-function crearTarea(listaTareas){
+const crearTarea = (listaTareas) => {
     let SeccionTareas = document.querySelector(".tareas")
     SeccionTareas.innerHTML = ""
 
@@ -34,7 +30,6 @@ function crearTarea(listaTareas){
         <button class="btn-del">Borrar tarea</button>
         <span class="span-completed">Completado: </span>
         <input type="checkbox" class="check-complete">
-
         `
         //Funcionalidad del boton de borrar tareas
         const btnBorrar = articleTarea.querySelector(".btn-del")
@@ -50,6 +45,7 @@ function crearTarea(listaTareas){
             articleTarea.classList.add("tarea-completada")
             checkComplete.disabled = true
             localStorage.setItem("tareas", JSON.stringify(tareas))
+            btnBorrar.disabled = false
         })
 
         //Se verifica si la tarea ya esta completada para pasarla a la seccion de "Completados"
@@ -57,6 +53,7 @@ function crearTarea(listaTareas){
             checkComplete.checked = true
             articleTarea.classList.add("tarea-completada")
             checkComplete.disabled = true
+            btnBorrar.disabled = false
         }
 
         SeccionTareas.appendChild(articleTarea)
@@ -65,18 +62,25 @@ function crearTarea(listaTareas){
     localStorage.setItem("tareas", JSON.stringify(tareas))
 }
 
+const agregarTarea = () => {
+    let nuevaTarea = {id: tareas.length + 1, titulo: titleTarea.value, descripcion: descTarea.value, hora: timeTarea.value, completado: false}
+    tareas.push(nuevaTarea)
+    crearTarea(tareas)
+    localStorage.setItem("tareas", JSON.stringify(tareas))
+    titleTarea.value = ""
+    descTarea.value = ""
+    timeTarea.value = ""        
+    modalOculto.classList.add("oculto")
+}
+
 //Cuando se clickea el boton de agregar, se crea un nuevo objeto en el array en base a la informacion ingresada
 btnAgregarTarea.addEventListener("click", () => {
-let nuevaTarea = {id: tareas.length + 1, titulo: titleTarea.value, descripcion: descTarea.value, hora: timeTarea.value, completado: false}
-tareas.push(nuevaTarea)
-crearTarea(tareas)
-localStorage.setItem("tareas", JSON.stringify(tareas))
-titleTarea.value = ""
-descTarea.value = ""
-timeTarea.value = ""        
-modalOculto.classList.add("oculto")
+    if(titleTarea.value == "" || descTarea.value == "" || timeTarea.value == ""){
+        alert("¡Los campos de la tarea estan vacios!")
+    }else{
+        agregarTarea()
+    }
 })
-
 
 btnAbrirModal.addEventListener("click", () => {
         modalOculto.classList.remove("oculto")
@@ -86,15 +90,26 @@ btnCerrarModal.addEventListener("click", () => {
     modalOculto.classList.add("oculto")
     })
 
+const seleccionarFiltro = (boton) => {
+    btnTodos.classList.remove("seleccion-filtro")
+    btnCompletados.classList.remove("seleccion-filtro")
+    btnNoCompletados.classList.remove("seleccion-filtro")
+    boton.classList.add("seleccion-filtro")
+}
 
 btnTodos.addEventListener("click", () => {
     crearTarea(tareas)
+    seleccionarFiltro(btnTodos)
 })
 
 btnCompletados.addEventListener("click", () => {
     crearTarea(tareas.filter(t => t.completado === true))
+    seleccionarFiltro(btnCompletados)
 })
 
 btnNoCompletados.addEventListener("click", () => {
     crearTarea(tareas.filter(t => t.completado === false))
+    seleccionarFiltro(btnNoCompletados)
 })
+
+crearTarea(tareas)
